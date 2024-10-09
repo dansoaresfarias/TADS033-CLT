878a5d76c8bf6d3bad13ba66b299a86432e01938
Danilo Farias <danilo.silva@nutes.ufpe.br>
2024-10-09 10:10:42 -0300

    Aula de JS

--- AulasJS/js/curriculoBoot.js
+++ AulasJS/js/curriculoBoot.js
@@ -4,14 +4,30 @@ function mudarJordy() {
     textoFreela.innerHTML = "Entre em contato comigo, sou melhor que Danilo. Jordy Negão!";
     textoFreela.setAttribute("class", "jordy");
 
-    let aJordy = document.createElement("a");
-    aJordy.setAttribute("href", "https://www.linkedin.com/in/jordy-arlego-82694b2b8/?originalSubdomain=br");
-    aJordy.setAttribute("target", "_blank");    
-    aJordy.innerHTML = "Linkedin do Negão 🍻";
+    if (document.getElementById("aJordy")) {
+        console.log(document.getElementById("aJordy"))
+    } else {
+        let aJordy = document.createElement("a");
+        aJordy.setAttribute("href", "https://www.linkedin.com/in/jordy-arlego-82694b2b8/?originalSubdomain=br");
+        aJordy.setAttribute("target", "_blank");
+        aJordy.innerHTML = "Linkedin do Negão 🍻";
+        aJordy.setAttribute("id", "aJordy");
 
-    let divFreelance = document.getElementById("freelance");
-    divFreelance.appendChild(aJordy);
+        let divFreelance = document.getElementById("freelance");
+        divFreelance.appendChild(aJordy);
+    }
 
     document.getElementById("foto").setAttribute("src", "imgs/jordy.jpeg");
+}
 
+function exponenciacao() {
+    let base = parseInt(document.getElementById("base").value);
+    let exp = parseInt(document.getElementById("expoente").value);
+
+    let resultado = base**exp;
+
+    console.log(base + "^" + exp + " = " + resultado);
+
+    let tagReult = document.getElementById("resultado");
+    tagReult.innerHTML = "<br><b>Resultado: </b>" + resultado;
 }
